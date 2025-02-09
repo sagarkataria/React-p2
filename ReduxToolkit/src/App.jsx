280f16f97bb013625ca12ff8a83a977577f04b0e
sagarkataria <sagarsingh0110@gmail.com>
2025-02-09 22:12:42 +0530

    Initialize Redux Toolkit setup with Tailwind CSS integration and basic Todo functionality

---
+++ ReduxToolkit/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+
+function App() {
+
+
+  return (
+    <div className="text-center">
+      <AddTodo/>
+      <Todos/>
+    </div>
+  )
+}
+
+export default App
