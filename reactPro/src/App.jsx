f2573177e4e9e7b458f9e20870a06f145e21206f
sagarkataria <sagarsingh0110@gmail.com>
2025-02-10 22:14:07 +0530

    setup ENV and Appwrite

---
+++ reactPro/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APP_APPWRITE)
+
+  return (
+    <>
+      <h1>A blog app with appwrite</h1>
+    </>
+  )
+}
+
+export default App
---
+++ reactPro/src/config/config.js
@@ -0,0 +1,8 @@
+const config = {
+    appwriteUrl: String(import.meta.env.VITE_APP_APPWRITE)
+      appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID)
+      appwriteDatabaseId: String(import.meta.env.VITE_APPAPPWRITE_DATABASE_ID)
+      appwriteCollectionId: String(import.meta.env.VITE_APPAPPWRITE_COLLECTION_ID)
+      appwriteBucketId: String(import.meta.env.VITE_APPAPPWRITE_BUCKET_ID)
+}
+export default config;
